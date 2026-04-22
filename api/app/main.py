02fcc35975aa01511dbee1424427fad4b3c345ba
arthurlins7 <alg2@cesar.school>
2026-04-22 18:16:35 -0300

    fix: add vercel domain to CORS

--- api/app/main.py
+++ api/app/main.py
@@ -6,7 +6,12 @@ app = FastAPI(title='GeekCommerce Segments API')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000', 'http://localhost:3001'],
+    allow_origins=[
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'https://geek-commerce-segments.vercel.app',
+    ],
+    allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )
